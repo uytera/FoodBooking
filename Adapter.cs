db394fb62a76814c9f454c98fc41be3631d4682d
uytera <uytera12@yandex.ru>
2018-04-08 14:26:12 +0500

    Jol
    lk

---
+++ Adapter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FoodBooking
+{
+    class Adapter : BaseAdapter<string>
+    {
+        private List<string> list;
+
+        private Context context;
+
+        public Adapter(Context context, List<string> list)
+        {
+            this.list = list;
+
+            this.context = context;
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+
+        public override string this[int position]
+        {
+            get
+            {
+                return list[position];
+            }
+        }
+
+        
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+
+            if(view == null)
+            {
+                view = LayoutInflater.From(context).Inflate(Resource.Layout.Cafe, null, false);
+
+                TextView textView1 = view.FindViewById<TextView>(Resource.Id.textView1);
+                textView1.Text = list[position];
+            }
+            return view;
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Content;
+
+namespace FoodBooking
+{
+    [Activity(Label = "Кафе", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        ListView listView1;
+
+        List<string> list;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            list = new List<string>();
+
+            list.AddRange(new string[] { "Lul", "Lmao", "笑", "攻撃", "射撃", "爆弾", "零"});
+
+            listView1 = FindViewById<ListView>(Resource.Id.listView1);
+
+            Adapter adapter = new Adapter(this, list);
+
+            listView1.Adapter = adapter;
+
+            listView1.ItemClick += delegate
+            {
+                Intent intent = new Intent(this, typeof(Cafe1));
+                StartActivity(intent);
+            };
+        }
+    }
+}
+
